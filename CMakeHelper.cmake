b7da88f685a0a014beb659c29283e4902ea2f162
Vrglab <vrglab1@gmail.com>
2026-04-06 18:44:36 +0200

    Finished project gen

--- CMakeHelper.cmake
+++ CMakeHelper.cmake
@@ -1,15 +1,22 @@
 SET(STANDARD_LIBS ZLIB::ZLIB spdlog::spdlog)
 
 # Function to collect .cpp files in the src directory
-function(collect_cpp_files DIR)
-    file(GLOB_RECURSE CPP_FILES "${DIR}/*.cpp" "${DIR}/*.c")
-    set(SRC_FILES ${CPP_FILES} PARENT_SCOPE)
+function(collect_cpp_files DIR OUT_VAR)
+    file(GLOB_RECURSE CPP_FILES
+            CONFIGURE_DEPENDS
+            "${DIR}/*.cpp"
+            "${DIR}/*.c"
+    )
+    set(${OUT_VAR} ${CPP_FILES} PARENT_SCOPE)
 endfunction()
 
-# Function to collect .h files in the include directory
-function(collect_h_files DIR)
-    file(GLOB_RECURSE H_FILES "${DIR}/*.h" "${DIR}/*.hpp" "${DIR}/**/*.h" "${DIR}/**/*.hpp")
-    set(INCLUDE_FILES ${H_FILES} PARENT_SCOPE)
+function(collect_h_files DIR OUT_VAR)
+    file(GLOB_RECURSE H_FILES
+            CONFIGURE_DEPENDS
+            "${DIR}/*.h"
+            "${DIR}/*.hpp"
+    )
+    set(${OUT_VAR} ${H_FILES} PARENT_SCOPE)
 endfunction()
 
 
---
+++ editor/include/soulless/pch.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+// C++ standard library
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstdint>
+#include <filesystem>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// Third-party headers you use all over the engine
+#include <spdlog/spdlog.h>
