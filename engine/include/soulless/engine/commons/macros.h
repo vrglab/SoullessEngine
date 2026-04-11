17fcc83a2bbac9d05b1bbc83592e598867628096
Vrglab <vrglab1@gmail.com>
2026-04-11 13:41:53 +0200

    SL-3 Implemented export macros

---
+++ engine/include/soulless/engine/commons/macros.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#ifdef _WIN32
+    #ifdef SOULLESS_ENGINE_EXPORTS
+        #define SOULLESS_API __declspec(dllexport)
+    #else
+        #define SOULLESS_API __declspec(dllimport)
+    #endif
+#else
+#define SOULLESS_API
+#endif
+
+#define SOULLESS_EXTERN extern "C" SOULLESS_API
